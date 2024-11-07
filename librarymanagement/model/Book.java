4537ddb3a74c8ae9494384b2b7b5380c4eefdf9b
Areeba khan <135821507+areeba090@users.noreply.github.com>
2024-11-07 22:37:43 +0500

    use case gui

---
+++ librarymanagement/model/Book.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.librarymanagement.model;
+
+public class Book {
+    private String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+}
+
+
+
